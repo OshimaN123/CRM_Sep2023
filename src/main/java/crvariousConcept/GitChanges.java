3bf8e5342fb7c29730c23bfa167c28e61b4b40e7
Oshima <mail.naushin@gmail.com>
2023-02-20 19:20:16 -0600

    new changes

--- src/main/java/crvariousConcept/GitChanges.java
+++ src/main/java/crvariousConcept/GitChanges.java
@@ -3,6 +3,19 @@ package crvariousConcept;
 public class GitChanges {
 	public static void main(String[] args) {
 		System.out.println("Git Test");
+		System.out.println("I am doing good");
+	}
+	public void printMe() {
+		System.out.println("Git test 1");
+	}
+	public void printMe1() {
+		System.out.println("Git test 2");
+	}
+	public void printMe2() {
+		System.out.println("Git test 3");
+	}
+	public void printMe3() {
+		System.out.println("Git test 4");
 	}
 
 }
